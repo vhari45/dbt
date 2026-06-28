14c06bfc71cd329be724513a12824aa5365cf9d6
harish veerla <veerlaharish45@gmail.com>
2026-06-28 17:27:30 +0000

    commit

--- models/Staging/incrementalmodel.sql
+++ models/Staging/incrementalmodel.sql
@@ -1,19 +1,20 @@
-
 {{
-    config(materialize = 'incremental',unique_key = 'order_id')
+    config(
+        materialized='incremental',
+        unique_key='order_id'
+    )
 }}
 
-
 select
-order_id,
-customerid,
-order_date,
-amount
-from {{source ('raw','orders')}}
+    order_id,
+    customerid,
+    order_date,
+    amount
+from {{ source('jaffle_shop', 'orders') }}
 
-{
-% if is_incremental() %
-
-}
-where order_date > (select max(order_date)) from {{this}}
+{% if is_incremental() %}
+where order_date > (
+    select max(order_date)
+    from {{ this }}
+)
 {% endif %}
